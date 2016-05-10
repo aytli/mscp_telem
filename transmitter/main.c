41760c1e8ca1d2e08543e86f25be6d54c1abb861
Andy Li <aytli@live.com>
2016-05-10 18:45:01 -0400

    added x macro for canbus packets

--- transmitter/main.c
+++ transmitter/main.c
@@ -2,12 +2,29 @@
 
 #define LED_PIN PIN_A1
 
+enum
+{
+    CAN_ID_TABLE(EXPAND_AS_ENUMERATION)
+};
+
+void send_data(int page_id, int len, int * data)
+{
+    int i;
+    putc(page_id);
+    putc(len);
+    for (i = 0 ; i < len ; i++)
+    {
+        putc(*(data+i));
+    }
+}
+
 void main()
 {
     delay_ms(100);
     putc('b');
     putc('b');
     delay_ms(100);
+    
     while(true)
     {
         putc(0x78);
--- transmitter/main.h
+++ transmitter/main.h
@@ -10,3 +10,16 @@
 #use delay(clock=20000000)
 #use rs232(baud = 9600, xmit = PIN_C6, rcv = PIN_C7)
 
+#define EXPAND_AS_ENUMERATION(a,b,c) a = b,
+
+// Table of CANbus IDs for the motor controller and bps
+//        Packet name      ,    ID, Length
+#define CAN_ID_TABLE(ENTRY)             \
+    ENTRY(MOTOR_BUS_VI     , 0x402,  8) \
+    ENTRY(MOTOR_VELOCITY   , 0x403,  8) \
+    ENTRY(MOTOR_TEMPERATURE, 0x40B,  8) \
+    ENTRY(BPS_VOLTAGE      , 0x500, 60) \
+    ENTRY(BPS_TEMPERATURE  , 0x501, 24) \
+    ENTRY(BPS_CURRENT      , 0x502,  2) \
+    ENTRY(BPS_BALANCING    , 0x503,  4) \
+    ENTRY(BPS_STATUS       , 0x504,  2)
